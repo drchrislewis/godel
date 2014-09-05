de407f2ffe55e26c370b9a329e7c4d6a5484d819
ros-industrial <ros-industrial@swri.org>
2014-09-05 11:14:39 -0500

    shaun reworked the launch file using namespaces so that joint_states coming from controller are now motoman/joint_states Added a shell script to reset the joint states associted with the target and camera to wrong values to demo calibration removed a stray "s" from the original planning and exectution launch file Added a color grid.dae to show the circle grid

---
+++ godel_sia20d_cal_moveit_config/config/reset_mutable_joints.sh
@@ -0,0 +1,59 @@
+ rosservice call /set_mutable_joint_states "joint_names:
+- 'asus_link_x_joint'
+joint_values:
+- 0.1"
+
+ rosservice call /set_mutable_joint_states "joint_names:
+- 'asus_link_y_joint'
+joint_values:
+- -0.0"
+
+ rosservice call /set_mutable_joint_states "joint_names:
+- 'asus_link_z_joint'
+joint_values:
+- 0.00"
+
+ rosservice call /set_mutable_joint_states "joint_names:
+- 'asus_link_roll_joint'
+joint_values:
+- 3.14"
+
+ rosservice call /set_mutable_joint_states "joint_names:
+- 'asus_link_pitch_joint'
+joint_values:
+- 0.0"
+
+ rosservice call /set_mutable_joint_states "joint_names:
+- 'asus_link_yaw_joint'
+joint_values:
+- 0.0"
+
+ rosservice call /set_mutable_joint_states "joint_names:
+- 'zcirclegrid7x5_x_joint'
+joint_values:
+- 0.90"
+
+ rosservice call /set_mutable_joint_states "joint_names:
+- 'zcirclegrid7x5_y_joint'
+joint_values:
+- 0.025"
+
+ rosservice call /set_mutable_joint_states "joint_names:
+- 'zcirclegrid7x5_z_joint'
+joint_values:
+- 0.42"
+
+ rosservice call /set_mutable_joint_states "joint_names:
+- 'zcirclegrid7x5_roll_joint'
+joint_values:
+- -0.0"
+
+ rosservice call /set_mutable_joint_states "joint_names:
+- 'zcirclegrid7x5_pitch_joint'
+joint_values:
+- 2.0"
+
+ rosservice call /set_mutable_joint_states "joint_names:
+- 'zcirclegrid7x5_yaw_joint'
+joint_values:
+- 1.1"
